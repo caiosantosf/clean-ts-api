165f87e8f493721d337d30d9d8d20dffc3a8a364
Caio Santos <caiosantosfreitas@hotmail.com>
2020-08-31 19:41:09 -0300

    refatorado o mongohelper para conectar novamente se estiver desconectado

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,7 +11,7 @@ describe('Account Repository MongoDb', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
