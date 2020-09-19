fd9a04601f48cd9febff57ab47be57698d5607ee
Caio Santos <caiosantosfreitas@hotmail.com>
2020-09-19 13:57:21 -0300

    rafatorado server erros para receber a stack do erro

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -145,7 +145,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError(null))
   })
 
   test('should return 400 if password confirmation fails', async () => {
@@ -197,7 +197,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError(null))
   })
 
   test('should return 200 if valid data is provided', async () => {
